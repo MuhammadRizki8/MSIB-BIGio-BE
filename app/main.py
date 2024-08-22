1bd14252f2ba13b2a18691f3d7a38f62fbe2c787
MuhammadRizki8 <muhammadrizki@upi.edu>
2024-08-22 11:55:22 +0700

    add cors

--- app/main.py
+++ app/main.py
@@ -4,9 +4,16 @@ from sqlalchemy.orm import Session
 from . import models, schemas, crud
 from .database import engine, Base, get_db
 from typing import List, Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
 @app.post("/stories/")
